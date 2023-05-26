2fca74afa9d815bebfad95dbc1521a41ad365cee
Kawamoto Ryoji <106289714+KawamotoRyou@users.noreply.github.com>
2023-05-26 12:41:45 +0000

    Commit 24 some bug fixes

--- OurWebApp/PrototypeWebapp.cs
+++ OurWebApp/PrototypeWebapp.cs
@@ -9,38 +9,20 @@ namespace MyNamespace
 {
     public static class MyFunction
     {
-        [FunctionName("Home")]
-        public static IActionResult Home(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "")] HttpRequest req,
+        [FunctionName("MyFunction")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("Home function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string responseMessage = "<html><body><h1>Welcome to my Azure Functions web app!</h1></body></html>";
+            string responseMessage = "<html><body><h1>Hello World!</h1></body></html>";
 
             return new ContentResult
             {
                 Content = responseMessage,
                 ContentType = "text/html",
-                StatusCode = StatusCodes.Status200OK
-            };
-        }
-
-        [FunctionName("Hello")]
-        public static IActionResult Hello(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "hello/{name}")] HttpRequest req,
-            string name,
-            ILogger log)
-        {
-            log.LogInformation($"Hello function processed a request for {name}.");
-
-            string responseMessage = $"<html><body><h1>Hello, {name}!</h1></body></html>";
-
-            return new ContentResult
-            {
-                Content = responseMessage,
-                ContentType = "text/html",
-                StatusCode = StatusCodes.Status200OK
+                StatusCode = 200
             };
         }
     }
