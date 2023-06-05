20c9dc6b935ecabba2edc9caa6e4fcaa9945a553
Jag1610 <coolkidcoby04@gmail.com>
2023-06-05 13:34:46 +0800

    Added a Create an Account HTML Page Commit 70

---
+++ OurWebAppTest/Views/Shared/_Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OurWebAppTest.Views.Shared
+{
+    public class _DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
