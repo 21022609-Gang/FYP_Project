c421edcda51382a08dbea422239111e7c515caa1
Jag1610 <coolkidcoby04@gmail.com>
2023-08-06 21:53:13 +0800

    added Search options for Jobpage

--- OurWebAppTest/Controllers/JobController.cs
+++ OurWebAppTest/Controllers/JobController.cs
@@ -1,10 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OurWebAppTest.Models;
+using RP.SOI.DotNet.Utils;
+using System.Data;
 
 namespace OurWebAppTest.Controllers
 {
     public class JobController : Controller
     {
         public IActionResult JobPage()
+        {
+            string sql = @"SELECT * FROM `Job Listing`";
+            DataTable ds = DBUtl.GetTable(sql);
+            int check = ds.Rows.Count;
+
+            List<string?> locations = ds.AsEnumerable()
+                .Select(x => x.Field<string>("Location")).ToList();
+
+            List<SelectListItem> locationList = locations
+                .Select(x => new SelectListItem
+                {
+                    Text = x,
+                    Value = x
+                }).ToList();
+
+            ViewData["LocationList"] = locationList;
+
+            return View("Job");
+        }
+
+        [HttpPost] // maybe use?
+        public IActionResult JobPage(JobListing jobL)
         {
             return View("Job");
         }
